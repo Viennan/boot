6fd0ef1d1de5471f2c937fa69331a78ff7b04ef6
Viennan <wangynine@163.com>
2024-02-17 23:09:57 +0800

    fix add prefix bug

--- 3rd/vulkan/install.sh
+++ 3rd/vulkan/install.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then # 判断当前用户是否有管理员权限（非roo
 fi
 
 # install vulkan sdk
-wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo_prefix tee /etc/apt/trusted.gpg.d/lunarg.asc
-sudo_prefix wget -qO /etc/apt/sources.list.d/lunarg-vulkan-$1-$2.list https://packages.lunarg.com/vulkan/$1/lunarg-vulkan-$1-$2.list
-sudo_prefix apt update
-sudo_prefix apt install vulkan-sdk
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | $sudo_prefix tee /etc/apt/trusted.gpg.d/lunarg.asc
+$sudo_prefix wget -qO /etc/apt/sources.list.d/lunarg-vulkan-$1-$2.list https://packages.lunarg.com/vulkan/$1/lunarg-vulkan-$1-$2.list
+$sudo_prefix apt update
+$sudo_prefix apt install vulkan-sdk
--- basic/init_c.sh
+++ basic/init_c.sh
@@ -7,4 +7,4 @@ if [[ $EUID -ne 0 ]]; then # 判断当前用户是否有管理员权限（非roo
     sudo_prefix="sudo" # 若没有管理员权限则在命令前面添加"sudo "
 fi
 
-sudo_prefix apt install -y build-essential cmake wget git unzip
+$sudo_prefix apt install -y build-essential cmake wget git unzip
